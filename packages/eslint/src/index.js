c6e75ede4c96e1722f0a2ccef9088f3746b62212
Guru Prasath Anandapadmanaban <98056+aprasadh@users.noreply.github.com>
2023-11-19 16:45:34 +0000

    Added eslint plugin workspace

---
+++ packages/eslint/src/index.js
@@ -0,0 +1,8 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+module.exports.rules = {
+    "ban-files": require("./ban-files"),
+    "no-implicit-svg-focusable": require("./no-implicit-svg-focusable"),
+    "prefer-live-region": require("./prefer-live-region"),
+};
+  
